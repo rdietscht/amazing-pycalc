d5376b20ff008f11d5a0a58b0fd64b184efd02e0
rdietscht <rdietscht@gmail.com>
2025-10-04 13:34:26 -0400

    creating method stubs

---
+++ calculator.py
@@ -0,0 +1,24 @@
+"""Simple Python module used for math computations.
+
+    Authors: Rafael D. and [Your Name]
+
+"""
+
+
+def prompt_user(opts):
+
+    print("Welcome to the calculator.")
+    print()
+    print("Select an option:\n")
+    print(f"\tOption {opt[0]}: {opt[1]}")
+
+
+def main():
+
+    user_in = input().upper()
+    while (user_in != 'Q'):
+        input(
+
+
+if __name__ == '__main__':
+    main()
